ad3e94594a70a98d3fdde587b12d4b49abff8f23
hamzatamry <hamzatamry@outlook.fr>
2022-12-12 08:51:40 +0100

    Firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'attendancetracker-5e615',
+    appId: '1:1056928144640:web:c885c373eeb3a944dfe7e5',
+    databaseURL: 'https://attendancetracker-5e615-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'attendancetracker-5e615.appspot.com',
+    apiKey: 'AIzaSyDnWL-YTVJbrXXYqiMc4Dp8kAlV5s9fH5g',
+    authDomain: 'attendancetracker-5e615.firebaseapp.com',
+    messagingSenderId: '1056928144640',
+  },
   production: true
 };
